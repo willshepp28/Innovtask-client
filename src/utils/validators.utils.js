ae7c8b2e6f189c522556194bf9a552790b8b43cd
willshepp28 <willsheppard29@gmail.com>
2023-08-07 18:04:09 -0400

    included custom validators for input fields

---
+++ src/utils/validators.utils.js
@@ -0,0 +1,10 @@
+// validationUtils.js
+
+export const isValidEmail = (email) => 
+  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email);
+
+export const isValidName = (name) => 
+  /^[a-zA-Z-'\s]+$/.test(name);
+
+export const isValidPassword = (password) => 
+  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
