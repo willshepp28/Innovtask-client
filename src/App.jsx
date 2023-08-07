9fa74c49baab6dde5badcd0303452c3b4fa4edd2
willshepp28 <willsheppard29@gmail.com>
2023-08-07 18:36:43 -0400

    install react-router-dom; created Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Signup from "./components/Signup";
+import Home from './components/Home';
 
 function App() {
   return (
-    <>
-      {/* <h1>Innovtask</h1> */}
-      <Signup />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
